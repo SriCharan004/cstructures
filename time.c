bf2bd8b854593a80fd22e93952c4bf0861564fbc
SriCharan004 <121278415+SriCharan004@users.noreply.github.com>
2023-12-06 13:02:13 +0530

    added changes to time.c

--- time.c
+++ time.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
+//structure for time
 struct Time {
     int hours;
     int minutes;
     int seconds;
 };
-
+// to add hours
 void addHours(struct Time *t, int additionalHours) {
     t->hours += additionalHours;
 }
 
+//to add minutes
 void addMinutes(struct Time *t, int additionalMinutes) {
     t->minutes += additionalMinutes;
     if (t->minutes >= 60) {
@@ -18,6 +20,8 @@ void addMinutes(struct Time *t, int additionalMinutes) {
     }
 }
 
+
+//to add seconds
 void addSeconds(struct Time *t, int additionalSeconds) {
     t->seconds += additionalSeconds;
     if (t->seconds >= 60) {
@@ -30,21 +34,28 @@ void addSeconds(struct Time *t, int additionalSeconds) {
     }
 }
 
+
+// Adding time
 struct Time addTime(struct Time t1, struct Time t2) {
-    struct Time result;
+    struct Time result; //Result
     result.seconds = t1.seconds + t2.seconds;
-    result.minutes = t1.minutes + t2.minutes + (result.seconds / 60);
-    result.hours = t1.hours + t2.hours + (result.minutes / 60);
-    result.seconds %= 60;
-    result.minutes %= 60;
+    result.minutes = t1.minutes + t2.minutes + (result.seconds / 60); //adding the excess 60 seconds to minutes
+    result.hours = t1.hours + t2.hours + (result.minutes / 60); //adding the excess 60 minutes to hours
+    result.seconds %= 60; // modulus of 60 seconds
+    result.minutes %= 60; // modulus of 60 minutes
     return result;
 }
 
-struct Time subtractTime(struct Time t1, struct Time t2) {
+struct Time subtractTime(struct Time t1, struct Time t2) { //caluculate the absolute time difference
     struct Time result;
-    int totalSeconds1 = t1.seconds + t1.minutes * 60 + t1.hours * 3600;
+//  Total seconds of two time structures
+    int totalSeconds1 = t1.seconds + t1.minutes * 60 + t1.hours * 3600; 
     int totalSeconds2 = t2.seconds + t2.minutes * 60 + t2.hours * 3600;
+
+//Difference
     int diffSeconds = totalSeconds1 - totalSeconds2;
+
+//if negative
     if (diffSeconds < 0)
         diffSeconds = -diffSeconds;
     result.hours = diffSeconds / 3600;
@@ -54,14 +65,16 @@ struct Time subtractTime(struct Time t1, struct Time t2) {
 }
 
 int main() {
-    struct Time time1 = {10, 30, 45};
-    struct Time time2 = {2, 15, 20};
 
-    addHours(&time1, 5);
-    addMinutes(&time1, 20);
-    addSeconds(&time1, 15);
+    struct Time time1 ;
+    struct Time time2 ;
+    
+    printf("Enter time1 (hours minutes seconds): ");
+    scanf("%d %d %d", &time1.hours, &time1.minutes, &time1.seconds);
 
-    printf("Updated Time 1: %d hours, %d minutes, %d seconds\n", time1.hours, time1.minutes, time1.seconds);
+    printf("Enter time2 (hours minutes seconds): ");
+    scanf("%d %d %d", &time2.hours, &time2.minutes, &time2.seconds);
+
 
     struct Time addedTime = addTime(time1, time2);
     printf("Time 1 + Time 2: %d hours, %d minutes, %d seconds\n", addedTime.hours, addedTime.minutes, addedTime.seconds);
