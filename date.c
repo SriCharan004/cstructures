469302605bd3abf89b1648b934ba10d3e9023a37
SriCharan004 <121278415+SriCharan004@users.noreply.github.com>
2023-12-12 14:01:01 +0530

    added coments to date.c

--- date.c
+++ date.c
@@ -9,30 +9,30 @@ struct Date
 
 int isLeapYear(int year) 
 {
-    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)); // formula for leap year
 }
 
 struct Date calculateTomorrow(struct Date currentDate) 
 {
     int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    currentDate.day++;
+    currentDate.day++; // add the day
 
     if (currentDate.month == 2 && isLeapYear(currentDate.year)) {
-        daysInMonth[2] = 29;
+        daysInMonth[2] = 29; //only if it is leap year, change the number in daysInMonth[]
     }
 
-    if (currentDate.day > daysInMonth[currentDate.month]) {
-        currentDate.day = 1;
-        currentDate.month++;
+    if (currentDate.day > daysInMonth[currentDate.month]) { // if we are exceeding the given days
+        currentDate.day = 1; // we are entering into a new month
+        currentDate.month++; // adding the month
 
-        if (currentDate.month > 12) {
+        if (currentDate.month > 12) { // if we are in december we have to move to Jan in next year
             currentDate.month = 1;
             currentDate.year++;
         }
     }
 
-    return currentDate;
+    return currentDate; //gives the pointer
 }
 
 int main() 
@@ -44,7 +44,7 @@ int main()
 
     tomorrow = calculateTomorrow(today);
 
-    printf("Tomorrow's date is: %02d-%02d-%d\n", tomorrow.day, tomorrow.month, tomorrow.year);
+    printf("Tomorrow's date is: %02d-%02d-%d\n", tomorrow.day, tomorrow.month, tomorrow.year); //format
 
     return 0;
 }
