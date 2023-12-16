5c61f644ac9d0baa05408dfafbde7f79352c0cc2
SriCharan004 <121278415+SriCharan004@users.noreply.github.com>
2023-12-16 15:04:34 +0530

    added insert at end

--- linkedlist/linklist.c
+++ linkedlist/linklist.c
@@ -11,8 +11,12 @@ struct node{
 
 typedef struct node Node;
 //Display Function
-struct node * insertatend(struct node *head,char c);
+struct node *insertatend(Node* head, char c);
 
+struct node *mloc(Node *t) {
+    t = (struct node *)malloc(sizeof(struct node));
+    return t;
+}
 
 int count(struct node *head);
 
@@ -33,13 +37,7 @@ void displaylinkedlist(struct node *H){
 		}
 }
 
-//Function to allocate memories
 
-struct node *mloc(Node *t) {
-    t = (struct node *)malloc(sizeof(struct node));
-    return t;
-
-}
 
 
 int main()
@@ -47,14 +45,14 @@ int main()
 	Node *head=NULL;
 	
 	Node *first,*second,*third,*fourth;
+
+	first=mloc(first);
+	second = mloc(second);
+	third = mloc(third);
+	fourth = mloc(fourth);
 	
-	first = (struct node *) malloc(sizeof(struct node));
-	second = (struct node *) malloc(sizeof(struct node));
-	third = (struct node *) malloc(sizeof(struct node));
-	fourth = (struct node *) malloc(sizeof(struct node));
 	
-	
-	first->data='A';
+	first->data='K';
 	first->next=second;
 	second->data='B';
 	second->next=third;
@@ -68,51 +66,60 @@ int main()
 
 	displaylinkedlist(head);
 	printf("Total nummber of nodes are %d\n",count(head));
-}
-
-
-
-
-/*struct node *mloc(const char *name) {
-    struct node *new_node = (struct node *)malloc(sizeof(struct node));
-    if (new_node == NULL) {
-        printf("Memory allocation failed!\n");
-        return NULL;
-    }
-    return new_node;
-
-}
-*/
-/*struct node * insertatend(struct node *head,char c)
-	{
-		struct node *temp;
-		temp = (struct node *) malloc(sizeof(struct node));
-		temp=head;
 	
-		if (head==NULL)
-			{
-				Node *first;			
-				first = (struct node *) malloc(sizeof(struct node));
-				first->data=c;
-				first->next=NULL;
-				head=first;
-				return head;
-			}
-		else {
-			while(head->next!=NULL)
-				{
-					head=head->next;
-				}
-			
-			struct node*last;
-			last = (struct node *) malloc(sizeof(struct node));
-			last->data=c;
-			head=last;
-			return temp;
+	
+	Node *T;
+	T=mloc(T);
+	T=NULL;
+	T=insertatend(T,'P');
+	displaylinkedlist(T);
+	printf("Total nummber of nodes are %d\n",count(T));
+	
+	
+	
+	
+}
+
+
+
+struct node *insertatend(Node* head, char c) 
+{
+	Node* tail=NULL;
+	tail=mloc(tail);	
+
+//allocation
+	if (tail!=NULL)
+		{	
+			tail->data=c;
+			tail->next=NULL;
 		}
+  
+//if it is empty
+	if (head==NULL)
+	{
+		return tail;
 	}
 
-*/
+	
+
+//else, we traverse
+	else{
+	Node *curr=head; //A temp variable
+
+	while(curr->next!=NULL)
+		{
+		curr=curr->next;
+		}
+//insertion
+	
+	curr->next=tail;
+	
+		return curr;
+	}
+}
+
+
+
 
 int count(struct node *h){
 	
@@ -125,15 +132,6 @@ int count(struct node *h){
 		}
 	return i+1;
 }
-
-
-
-
-
-
-
-
-
 
 
 
