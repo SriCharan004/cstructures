a8960dbf487137401afd26c70f13a0107397329f
SriCharan004 <121278415+SriCharan004@users.noreply.github.com>
2024-02-17 09:10:40 +0000

    added deline.c

--- filehandling/delline.c
+++ filehandling/delline.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
     FILE *fp, *temp;
     char ch;
     int del_line, temp_line;
 
-    // Open the file in read mode
     fp = fopen("input.txt", "r");
     if (fp == NULL) {
         printf("File does not exist or cannot be opened.\n");
-        exit(1);
+        return 0;
     }
 
     // Open a temporary file in write mode
@@ -18,18 +16,17 @@ int main() {
     if (temp == NULL) {
         printf("Cannot create temporary file.\n");
         fclose(fp);
-        exit(1);
+        return 0;
     }
 
     // Ask the user for the line to delete
     printf("Enter line number to delete: ");
     scanf("%d", &del_line);
 
-    // Read each line from input file and copy to temporary file,
-    // except the line to be deleted
+    //read all lines except the delline
     temp_line = 1;
     while ((ch = fgetc(fp)) != EOF) { // till the last character
-        if (ch == '\n')
+        if (ch == '\n')// till the end of the line
             temp_line++;
         // If current line is not the one to delete, write to temporary file
         if (temp_line != del_line)
