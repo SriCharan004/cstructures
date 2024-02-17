aba1bcd521db7356d3f89701806bbce468ef4177
SriCharan004 <121278415+SriCharan004@users.noreply.github.com>
2024-02-17 09:03:45 +0000

    empty

--- filehandling/wc.c
+++ filehandling/wc.c
@@ -2,14 +2,9 @@
 
 int main(){
     
-    FILE *f1,*f2;
+    FILE *f1;
     f1=fopen("input.txt","r");
     if(f1==NULL){
-            return 0;
-    }
-    
-    f2=fopen("input.txt","r");
-    if(f2==NULL){
             return 0;
     }
 
@@ -19,16 +14,22 @@ int main(){
     while((c=fgetc(f1))!=EOF){
         countchar++;
     }
-    f1=f2;
+    
     printf("Total Characters are %d \n",countchar);
+    fclose(f1);
+
+    f1=fopen("input.txt","r");
+    if(f1==NULL){
+            return 0;
+    }
+
 
     while((c=fgetc(f1))!=EOF){
-        if(c=='\t'){
+        if(c == '\t'){
             counttab++;
         }
     }
-    f1=f2;
-    printf("Total Characters are %d \n",counttab);
+    printf("Total tabs are %d \n",counttab);
 
 
 }
